5198b7d1f954c20ad978b4da2a1bd864ef35dd87
NateWalchenbach <natewalchenbach24@gmail.com>
2023-08-17 16:52:30 -0700

    updates

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
-import { render } from "react-dom";
+import ReactDOM from "react-dom";
 
 import App from "./App";
 
 const rootElement = document.getElementById("root");
-render(<App />, rootElement);
+
+ReactDOM.render(<App />, rootElement);
